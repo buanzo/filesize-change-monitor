e771d6a467da4aeb0ed3493da2414523f2a299f6
buanzo <buanzo@buanzo.com.ar>
2020-04-21 11:16:53 -0300

    initial code commit, working

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # filesize-change-monitor
-This python3 script monitors filesizes and reports growth percent and other stats. Useful to detect which logfile is growing the fastest, to detect website being attacked or failing.
+
+This python3 script monitors filesizes and reports growth percent and other
+stats.  Useful to detect which logfile is growing the fastest, to detect
+website being attacked or failing.
+
+Requires python3. Other modules indicated in requirements.txt
+
+Feel free to PR.
+
+Cheers,
+Buanzo
---
+++ filesizemon.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# Usage Example: sizemon.py '/var/log/apache2/*access*log'
+# MAKE SURE YOU USE single quotes around path/pattern specification
+# If you dont use the single quotes, current script version will monitor only
+# the first bash-expanded filename.
+#
+# Author: Buanzo
+# License: GPLv3
+# Version: 0.1
+# Date: 4/21 [I was busy 4/20]
+#
+# THIS IS A QUICK AND DIRTY SCRIPT FULL OF BAD CODE
+#
+import glob
+import sys
+import os
+import time
+from pprint import pprint
+
+biggest_change = float(0)
+biggest_change_item = ''
+
+def get_change_stats(item,current,previous):
+  global biggest_change
+  global biggest_change_item
+  bn = os.path.basename(item)
+  if current == previous:
+    x= '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE=0%'.format(bn,current,previous)
+  else:
+    change_percent = ((float(current)-previous)/previous)*100
+    if change_percent > biggest_change:
+      biggest_change = change_percent
+      biggest_change_item = item
+    x = '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE={:f}%'.format(bn,current,previous,change_percent)
+  return(x)
+
+def logsizemon():
+  try:
+    pat = sys.argv[1]
+  except Exception:
+    print("Usage EXAMPLE: {} '/var/log/apache2/*access*log'".format(sys.argv[0]))
+    sys.exit(1)
+
+  i_sizes = {}
+  c_sizes = {}
+
+  # load initial file sizes
+  print('Loading initial sizes...')
+  for f in glob.glob(sys.argv[1]):
+    i_sizes[f]  = os.path.getsize(f)
+
+  # Initial wait
+  print('Waiting 10 seconds to begin monitoring...')
+  time.sleep(10)
+
+  while True:
+    # Get current file sizes
+    for f in glob.glob(sys.argv[1]):
+      # FIX: Include newly appeared files
+      c_sizes[f]  = os.path.getsize(f)
+    print("\033[2J\033[0;0H\033[0;37m")  # ANSI ftw
+    for item in i_sizes:
+      current = c_sizes[item]
+      previous = i_sizes[item]
+      print(get_change_stats(item,current,previous))
+    print("\033[1;33m------------------------------------------\033[0;37m")
+    print("\033[1;32mCURRENT BIGGEST CHANGE: {:f}% for {}\033[0;37m".format(biggest_change,biggest_change_item))
+    time.sleep(10)
+    
+
+if __name__ == '__main__':
+  logsizemon()
