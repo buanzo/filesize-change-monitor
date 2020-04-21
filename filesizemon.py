9c1a854b2f9545418333fc1b16fa702ae80c01fe
buanzo <buanzo@buanzo.com.ar>
2020-04-21 17:10:33 -0300

    pep8 enhancements 2spc->4spc tab

--- filesizemon.py
+++ filesizemon.py
@@ -22,57 +22,59 @@ bc_item = ''
 
 
 def get_change_stats(item, current, previous):
-  global bc
-  global bc_item
-  bn = os.path.basename(item)
-  if current == previous:
-    x = '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE=0%'.format(bn, current, previous)
-  else:
-    change_percent = ((float(current)-previous)/previous)*100
-    if change_percent > bc:
-      bc = change_percent
-      bc_item = item
-    x = '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE={:f}%'.format(bn,
-                                                            current,
-                                                            previous,
-                                                            change_percent)
-  return(x)
+    global bc
+    global bc_item
+    bn = os.path.basename(item)
+    if current == previous:
+        x = '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE=0%'.format(bn,
+                                                             current,
+                                                             previous)
+    else:
+        change_percent = ((float(current)-previous)/previous)*100
+        if change_percent > bc:
+            bc = change_percent
+            bc_item = item
+        x = '{:50}\tCUR={:10}\tORIG={:10}\tCHANGE={:f}%'.format(bn,
+                                                                current,
+                                                                previous,
+                                                                change_percent)
+    return(x)
 
 
 def logsizemon():
-  try:
-    pat = sys.argv[1]
-  except Exception:
-    print("Usage EXAMPLE: {} '/logs/*.log'".format(sys.argv[0]))
-    sys.exit(1)
+    try:
+        pat = sys.argv[1]
+    except Exception:
+        print("Usage EXAMPLE: {} '/logs/*.log'".format(sys.argv[0]))
+        sys.exit(1)
 
-  i_sizes = {}
-  c_sizes = {}
+    i_sizes = {}
+    c_sizes = {}
 
-  # load initial file sizes
-  print('Loading initial sizes...')
-  for f in glob.glob(sys.argv[1]):
-    i_sizes[f] = os.path.getsize(f)
-
-  # Initial wait
-  print('Waiting 10 seconds to begin monitoring...')
-  time.sleep(10)
-
-  while True:
-    # Get current file sizes
+    # load initial file sizes
+    print('Loading initial sizes...')
     for f in glob.glob(sys.argv[1]):
-      # FIX: Include newly appeared files
-      c_sizes[f] = os.path.getsize(f)
-    print("\033[2J\033[0;0H\033[0;37m")  # ANSI ftw
-    for item in i_sizes:
-      current = c_sizes[item]
-      previous = i_sizes[item]
-      print(get_change_stats(item, current, previous))
-    print("\033[1;33m------------------------------------------\033[0;37m")
-    print("\033[1;32mMAX CHANGE: {:f}% for {}\033[0;37m".format(bc,
-                                                                bc_item))
+        i_sizes[f] = os.path.getsize(f)
+
+    # Initial wait
+    print('Waiting 10 seconds to begin monitoring...')
     time.sleep(10)
+
+    while True:
+        # Get current file sizes
+        for f in glob.glob(sys.argv[1]):
+            # FIX: Include newly appeared files
+            c_sizes[f] = os.path.getsize(f)
+        print("\033[2J\033[0;0H\033[0;37m")    # ANSI ftw
+        for item in i_sizes:
+            current = c_sizes[item]
+            previous = i_sizes[item]
+            print(get_change_stats(item, current, previous))
+        print("\033[1;33m------------------------------------------\033[0;37m")
+        print("\033[1;32mMAX CHANGE: {:f}% for {}\033[0;37m".format(bc,
+                                                                    bc_item))
+        time.sleep(10)
 
 
 if __name__ == '__main__':
-  logsizemon()
+    logsizemon()
