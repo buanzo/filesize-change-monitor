49d33a4466b3de71ad8d4c564d47d6c38cad870e
buanzo <buanzo@buanzo.com.ar>
2020-04-28 21:55:32 -0300

    tqdm used for sleep if available, starting to structure the code for argument parsing

--- filesizemon.py
+++ filesizemon.py
@@ -21,6 +21,17 @@ bc = float(0)
 bc_item = None
 
 
+def espera(s):
+    try:
+        from tqdm import tqdm
+    except:
+        time.sleep(s)
+        return
+    with tqdm(total=s) as pbar:
+        for i in range(s):
+            time.sleep(1)
+            pbar.update(1)
+
 def get_change_stats(item, current, previous):
     global bc
     global bc_item
@@ -30,7 +41,11 @@ def get_change_stats(item, current, previous):
                                                              current,
                                                              previous)
     else:
-        change_percent = ((float(current)-previous)/previous)*100
+        if previous == 0:
+            # a file that grows from 0 to anything grows a 100%
+            change_percent = 100
+        else:
+            change_percent = ((float(current)-previous)/previous)*100
         if change_percent > bc:
             bc = change_percent
             bc_item = item
@@ -57,26 +72,46 @@ def logsizemon():
         i_sizes[f] = os.path.getsize(f)
 
     # Initial wait
-    print('Waiting 10 seconds to begin monitoring...')
-    time.sleep(10)
+    try:
+        print('Waiting 10 seconds to begin monitoring...')
+        espera(10)
+    except KeyboardInterrupt:
+        print('\nExiting...')
+        sys.exit(0)
 
-    while True:
-        # Get current file sizes
-        for f in glob.glob(sys.argv[1]):
-            # FIX: Include newly appeared files
-            c_sizes[f] = os.path.getsize(f)
-        print("\033[2J\033[0;0H\033[0;37m")    # ANSI ftw
-        for item in i_sizes:
-            current = c_sizes[item]
-            previous = i_sizes[item]
-            print(get_change_stats(item, current, previous))
-        print("\033[1;33m------------------------------------------\033[0;37m")
-        if bc_item is not None:
-            print("\033[1;32mMax Change:")
-            print("{:f}% for {}\033[0;37m".format(bc,
-                                                  bc_item))
-        time.sleep(10)
+    try:
+        while True:
+            # Get current file sizes
+            for f in glob.glob(sys.argv[1]):
+                c_sizes[f] = os.path.getsize(f)
+                if f not in i_sizes:
+                    # if the file just started existing
+                    # it will not be in i_sizes
+                    # Lets use initial size as 0:
+                    print('adding {}'.format(f))
+                    i_sizes[f] = 0
 
+            print("\033[2J\033[0;0H\033[0;37m")    # ANSI ftw
+
+            for item in i_sizes:
+                current = c_sizes[item]
+                previous = i_sizes[item]
+                print(get_change_stats(item, current, previous))
+
+            print("\033[1;33m------------------------------------------\033[0;37m")
+
+            if bc_item is not None:
+                print("\033[1;32mMax Change:")
+                print("{:f}% for {}\033[0;37m".format(bc,
+                                                      bc_item))
+            espera(10)
+            # TODO: add argparser
+            # TODO: Are we comparing against initial sizes, or sizes obtained in previous loop?
+            # TODO: if previous loop, then i_sizes = c_sizes
+            # TODO: if not, we leave i_sizes as initially created
+            
+    except KeyboardInterrupt:
+        print('\nExiting...')
 
 if __name__ == '__main__':
     logsizemon()
